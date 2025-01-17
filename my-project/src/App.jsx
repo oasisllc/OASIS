48523c5d98460c2a0df769861a74f2cb6d5e8a72
GameDevRocky <159648130+GameDevRocky@users.noreply.github.com>
2025-01-16 20:29:51 -0500

    Main

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ const App = () => {
+  return (
+    <div className='flex w-full justify-center bg-slate-600 '>
+      <h1>
+      Hello
+
+      </h1>
+
+
+    </div>
+  )
+}
+
+
+
+
+export default App;
