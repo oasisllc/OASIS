9378ed6e5e86bf8813a49cedbc9f3e39867f190c
Eliaslopes248 <ealopes@aggies.ncat.edu>
2025-01-17 01:45:36 -0500

    fixed nothing

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./pages/LandingPage/Hero";
 import './index.css'
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 
 export default function App() {
@@ -30,7 +32,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hero />}>
+        <Route path="/" element={<Hero />}/>
+          <Route path="/loginpage" element={<Login />} />
+          <Route path="/signuppage" element={<Login />} />
         
         
       </Routes>
