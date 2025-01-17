c176070a60fd064640b82f4f77226d36477add8b
Eliaslopes248 <ealopes@aggies.ncat.edu>
2025-01-17 03:08:32 -0500

    Revert "Merge branch 'main' of https://github.com/Eliaslopes248/Dreams_collective_hackathon"
    This reverts commit fce427bd12c3c5ce467dd077c81c9f0be3dde720, reversing
    changes made to 873dc367b4d46851440e9ba26a0ab4b046f1f5bb.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,28 +7,6 @@ import Signup from "./pages/Signup/Signup";
 
 
 export default function App() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 640);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  
-  const handleMouseMove = (event) => {
-    const normalizedX = (event.clientX / window.innerWidth) * 2 - 1;
-    const normalizedY = -(event.clientY / window.innerHeight) * 2 + 1;
-    setMousePosition({ x: normalizedX, y: normalizedY });
-  };
-
-
-
-
   return (
     <BrowserRouter>
       <Routes>
--- my-project/src/pages/LandingPage/Hero.jsx
+++ my-project/src/pages/LandingPage/Hero.jsx
@@ -1,16 +1,15 @@
 import { FaRegCalendarAlt } from "react-icons/fa";
 import './Hero.css';
-import { Canvas } from "@react-three/fiber";
-import CalendarModel from "../../../public/models/CalendarModel";
-import { OrbitControls } from "@react-three/drei";
+
+
 
 const NavItems = () => {
   const navLinks = ['About', 'Why Us', 'Blog', 'Clients'];
 
   return (
-    <ul className="flex space-x-8 text-lg font-medium">
+    <ul className="flex space-x-6">
       {navLinks.map((link, index) => (
-        <li key={index} className="cursor-pointer hover:text-purple-500 transition-colors duration-300">
+        <li key={index} className=" cursor-pointer">
           {link}
         </li>
       ))}
@@ -20,8 +19,8 @@ const NavItems = () => {
 
 const Navbar = () => {
   return (
-    <div className="navbar relative w-full h-16 top-0 px-8 flex justify-between items-center z-50">
-      <div className="flex items-center space-x-4 ">
+    <div className="navbar w-full h-16 flex relative top-0 px-8 justify-between items-center">
+      <div className="flex items-center space-x-4">
         <FaRegCalendarAlt size={30} />
         <h1 className="font-bold text-2xl">Appointly</h1>
       </div>
@@ -30,45 +29,18 @@ const Navbar = () => {
   );
 };
 
-const Hero = (props) => {
+const Hero = () => {
   return (
-    <div className=" hero w-full h-screen fixed " onMouseMove={props.handleMouseMove}>
+    <div className="hero w-full h-screen fixed">
       <Navbar />
-      <div className="mt-4 hero-content w-full flex items-center justify-center h-screen px-4 sm:px-16">
-        <div className="w-full h-full flex flex-col md:flex-row items-center  justify-between gap-8 p-12 border shadow-2xl shadow-yellow-300 border-yellow-200 rounded-3xl">
-          <div className="w-full sm:w-1/2 flex flex-col items-center md:items-start justify-center text-center md:text-left">
-            <header className="text-6xl p-2 md:bg-gradient-to-r bg-gradient-to-b from-yellow-100 to-yellow-300 bg-clip-text text-transparent md:text-6xl lg:text-7xl xl:text-8xl text-nowrap  font-bold">
-              Welcome To <br /> Appointly
-            </header>
-            <br/>
-            <h1 className="mt-4 px-2 font-serif text-nowrap lg:text-base md:text-sm text-xs">
-              Simplify your scheduling with ease and style.
-            </h1>
-            <div className="flex  justify-between">
+    <div className="hero w-full items-center h-screen flex flex-col p-4 rounded-xl">
+      <div className="w-full h-full border-2 border-neutral-900 rounded-3xl ">
 
-            <button className=" mt-6 px-6 py-3 font-semibold rounded-full shadow-sm transition-all duration-300">
-              Login
-            </button>
-            <button className="mt-6 visible md:hidden px-6 py-3 font-semibold rounded-full shadow-md  transition-all duration-300">
-              Register
-            </button>
-            </div>
-          </div>
+        
 
-          <div className=" md:relative fixed md:translate-y-0 translate-y-60 -z-50 w-full sm:w-1/2 h-56 md:h-96 flex items-center justify-center">
-            <Canvas className="w-full h-full xl:h-screen ">
-              <ambientLight intensity={0.5} />
-              <directionalLight intensity={1} position={[2, 2, 2]} />
-              <CalendarModel
-                mousePosition={props.mousePosition}
-                scale={props.isMobile? 2: 2.5}
-                isMobile={props.isMobile}
-              />
-              <OrbitControls />
-            </Canvas>
-          </div>
-        </div>
+      
       </div>
+    </div>
     </div>
   );
 };
